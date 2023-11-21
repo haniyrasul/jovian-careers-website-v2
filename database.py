aba4e293f3ee30a14ee48915cfbbbb96675a79eb
haniyrasul <mahaniyrasul27@gmail.com>
2023-11-21 20:33:52 +0530

    database update

--- database.py
+++ database.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 db_connection_string = os.getenv("DB_CONNECTION_STRING")
 
-engine = create_engine(db_connection_string)
+engine = create_engine(db_connection_string, echo=True)
 
 def load_jobs_from_db():
     with engine.connect() as conn:
@@ -26,3 +26,20 @@ def  load_job_from_db(id):
             return None
         else:
             return dict(rows[0]._asdict())
+
+
+def add_application_to_db(job_id, data):
+    with engine.connect() as conn:
+        query = text("INSERT INTO application (job_id, full_name, email, linkedin_url, education, work_experience, resume_url) VALUES (:job_id, :full_name, :email, :linkedin_url, :education, :work_experience, :resume_url)")
+
+        conn.execute(query, {
+            'job_id': job_id,
+            'full_name': data['full_name'],
+            'email': data['email'],
+            'linkedin_url': data['linkedin_url'],
+            'education': data['education'],
+            'work_experience': data['work_experience'],
+            'resume_url': data['resume_url']
+})
+        conn.commit() 
+
